3d7290b4580031e306dc31f27fbb4ad16fc458d2
root <root@dare-devil.dare-devil.kali.lan>
2025-08-21 12:31:03 -0700

    chore: Final cleanup and verification before redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This line is the key to the entire deployment.
   base: '/pumping-frequency-estimator/',
-
   plugins: [react()],
-
                             server: {
                               host: true,
-                            port: 3000,
+                              port: 3000,
                             },
 })
